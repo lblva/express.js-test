fc1688b9448546d0a97dfffcaf00e38289bb2691
lblva <lola.batchalova@hotmail.com>
2024-11-13 10:32:37 +0100

    testing

--- models/User.js
+++ models/User.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+    name: { type: String, required: true },
     email: { type: String, required: true},
+    password: { type: String, required: true},
+    profilePicture: { type: String, default: ''},
     createdAt: {type: Date, default: Date.now},
 });
 
