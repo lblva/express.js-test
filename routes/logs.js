4ab2d77c0616633776f539362692ee5f3db23568
lblva <lola.batchalova@hotmail.com>
2024-12-29 09:13:41 +0100

    last watered will also get returned

--- routes/logs.js
+++ routes/logs.js
@@ -29,28 +29,25 @@ router.get('/user/:userId/to-water', async (req, res) => {
         // Prepare plant watering data
         const plantWaterData = await Promise.all(
             user.plants.map(async (plant) => {
-                // Find the latest log for the plant
                 const log = await Log.findOne({ user: userId, plant: plant._id })
                     .sort({ wateredAt: -1 });
-
+        
                 const lastWatered = log ? log.wateredAt : null;
-                const validUntil = log ? log.validUntil : null;
-
-                const isWatered = validUntil && new Date(validUntil) > new Date();
-
                 const wateringFrequency = plant.water * 24 * 60 * 60 * 1000; // Water frequency in ms
                 const nextWateringDate = lastWatered
                     ? new Date(new Date(lastWatered).getTime() + wateringFrequency)
                     : null;
-
+        
                 return {
                     plantId: plant._id,
                     plantName: plant.name,
+                    lastWatered,  // Send the lastWatered date
                     nextWateringDate,
                     isWatered,
                 };
             })
         );
+        
 
         res.json(plantWaterData);
     } catch (error) {
